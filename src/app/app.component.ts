4b13d08210a368289bc1c36136569c619b59e645
PrasannaRajanS <prasanna555001@gmail.com>
2024-08-12 18:27:54 +0530

    Fetch:Section-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from './user-data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TraningProjec';
+  users = DUMMY_USERS
+
+  selectedId = "u1"
+
+
+  get selectedUser(){
+    return this.users.find((user)=>user.id === this.selectedId)
+  }
+
+  onSelectUser(id:string){
+    console.log(id);
+    this.selectedId = id
+  }
 }
